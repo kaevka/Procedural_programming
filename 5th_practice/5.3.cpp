5916cce61c9b0803ab7020f45e20495cb221efe4
kaevka <105744651+kaevka@users.noreply.github.com>
2022-10-26 03:31:09 +0300

    Update 5.3.cpp

--- 5th_practice/5.3.cpp
+++ 5th_practice/5.3.cpp
@@ -1,17 +1,14 @@
-//5.3
-// Ïðåîáðàçîâàíèå ðàçäåëèòåëåé: çàìåíà ïðîáåëîâ íà äðóãèå(îïðåäåëåííûå) ñèìâîëû.
-//My name is Eva Kapustyan
 #include <fstream>
 #include <iostream>
 using namespace std;
 int main()
 {
 	setlocale(0, "russian");
-	string path = "C:\\Users\\Åâà\\Desktop\\visual studio\\txt5.3.txt";
+	string path = "txt5.3.txt";
 	fstream f;
 	f.open(path, fstream::out | fstream::in | fstream::app);
 	char sym, bykva;
-	cout << "Íà êàêîé ñèìâîë Âû áû õîòåëè çàìåíèòü ïðîáåë?" << endl;
+	cout << "На какой символ Вы бы хотели заменить пробел?" << endl;
 	cin >> bykva;
 	char arr[60] = {};
 	int i = 0;
